c22b3561f1f31546147f70403e2c81b91c260306
alitaso345 <alice.maru345@gmail.com>
2020-01-07 22:33:03 +0900

    simple json

---
+++ simple/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "encoding/json"
+import "fmt"
+
+const text = `
+{
+	"foo": "hello",
+	"bar": "golang"
+}
+`
+
+func main() {
+	var m = make(map[string]string)
+	json.Unmarshal([]byte(text), &m)
+	fmt.Println(m["foo"], m["bar"])
+}
