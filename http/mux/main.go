8ac1449f33af027319496f80ccbd5f619f822f94
alitaso345 <alice.maru345@gmail.com>
2020-01-09 20:30:43 +0900

    mux

---
+++ http/mux/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+import "net/http"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello")
+}
+
+func main() {
+	handlers := http.NewServeMux()
+	handlers.HandleFunc("/1", handler)
+	server := &http.Server{Addr: ":8080", Handler: handlers}
+
+	go server.ListenAndServe()
+
+	handlers.HandleFunc("/2", handler)
+
+	select {}
+}
