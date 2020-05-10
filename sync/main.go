5a0da5f88a2b5a63d9fba2c0f9db403db2b979a0
alitaso345 <alice.maru345@gmail.com>
2020-05-10 13:35:05 +0900

    sync map sample

--- sync/main.go
+++ sync/main.go
@@ -2,20 +2,55 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"sync"
+	"time"
 )
 
-func main() {
-	var m sync.Map
+var userMap sync.Map
+
+func streamingAPI(quit chan<- bool) {
+	for i := 0; i < 5; i++ {
+		userMap.Range(func(key, val interface{}) bool {
+			ch, ok := val.(chan string)
+			if ok {
+				ch <- fmt.Sprintf("data: %d", i)
+			} else {
+				fmt.Printf("why error for %v\n", key)
+			}
+			return true
+		})
+		time.Sleep(1 * time.Second)
+	}
+	quit <- true
+}
+
+func addUser(user string) {
+	ch := make(chan string)
+	userMap.Store(user, ch)
 
 	go func() {
 		for {
-			m.Store(rand.Intn(100000), struct{}{})
+			select {
+			case msg := <-ch:
+				fmt.Printf("Handler %s: %s\n", user, msg)
+			}
 		}
 	}()
+}
 
-	for {
-		fmt.Println(m.Load(rand.Intn(100000)))
-	}
+func main() {
+	quit := make(chan bool)
+	go streamingAPI(quit)
+
+	addUser("foo")
+
+	time.Sleep(1 * time.Second)
+	addUser("bar")
+
+	time.Sleep(1 * time.Second)
+	addUser("baz")
+
+	time.Sleep(1 * time.Second)
+	addUser("quux")
+	<-quit
 }
