426cb2456f18e15544ea453dff4db1539560cf79
alitaso345 <alice.maru345@gmail.com>
2020-06-07 15:42:28 +0900

    get tweets server and client

--- twi-grpc/client/main.go
+++ twi-grpc/client/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	pb "github.com/alitaso345/zatsu/twi-grpc/proto"
 	"google.golang.org/grpc"
@@ -20,18 +22,24 @@ func main() {
 
 	client := pb.NewTimelineClient(conn)
 	hashTag := os.Args[1]
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	stream, err := client.Connect(ctx, &pb.Room{HashTag: hashTag})
+	stream, err := client.Connect(context.Background(), &pb.Room{HashTag: hashTag})
 	if err != nil {
 		log.Fatalf("failed to connect: %s", err)
 	}
+	defer stream.CloseSend()
 
-	for {
-		res, err := stream.Recv()
-		if err != nil {
-			log.Fatalln(err)
+	done := make(chan os.Signal)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			fmt.Println(fmt.Sprintf("@%s %s by %s\n", res.GetName(), res.GetMessage(), res.GetPlatformType()))
 		}
-		fmt.Println(fmt.Sprintf("@%s %s by %s\n", res.GetName(), res.GetMessage(), res.GetPlatformType()))
-	}
+	}()
+
+	<-done
 }
--- twi-grpc/server/main.go
+++ twi-grpc/server/main.go
@@ -1,23 +1,79 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
-	"time"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/kelseyhightower/envconfig"
 
 	pb "github.com/alitaso345/zatsu/twi-grpc/proto"
 	"google.golang.org/grpc"
 )
 
+type TwitterConfig struct {
+	ConsumerKey       string `envconfig:"CONSUMER_KEY"`
+	ConsumerSecret    string `envconfig:"CONSUMER_SECRET"`
+	AccessToken       string `envconfig:"ACCESS_TOKEN"`
+	AccessTokenSecret string `envconfig:"ACCESS_TOKEN_SECRET"`
+}
+
 type timelineService struct{}
 
 func (s *timelineService) Connect(req *pb.Room, stream pb.Timeline_ConnectServer) error {
+	twitterCh := func(done <-chan interface{}) <-chan *twitter.Tweet {
+		ch := make(chan *twitter.Tweet)
+		go func() {
+			defer func() {
+				log.Println("close twitter ch")
+				close(ch)
+			}()
+			var c TwitterConfig
+			envconfig.Process("TWITTER", &c)
+			config := oauth1.NewConfig(c.ConsumerKey, c.ConsumerSecret)
+			token := oauth1.NewToken(c.AccessToken, c.AccessTokenSecret)
+			httpClient := config.Client(oauth1.NoContext, token)
+
+			client := twitter.NewClient(httpClient)
+
+			demux := twitter.NewSwitchDemux()
+			demux.Tweet = func(tweet *twitter.Tweet) {
+				if tweet.RetweetedStatus != nil {
+					return
+				}
+				fmt.Println(fmt.Sprintf("%s\n", tweet.Text))
+				ch <- tweet
+			}
+
+			filterParams := &twitter.StreamFilterParams{Track: []string{req.GetHashTag()}}
+			twitterStream, err := client.Streams.Filter(filterParams)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer twitterStream.Stop()
+
+			go demux.HandleChan(twitterStream.Messages)
+			<-done
+			return
+		}()
+
+		return ch
+	}
+
+	done := make(chan interface{})
+	tc := twitterCh(done)
 	for {
-		err := stream.Send(&pb.Comment{Name: "alice", Message: req.GetHashTag(), PlatformType: pb.PlatformType_TWITTER})
-		if err != nil {
-			return err
+		select {
+		case msg := <-tc:
+			err := stream.Send(&pb.Comment{Name: msg.User.ScreenName, Message: msg.Text, PlatformType: pb.PlatformType_TWITTER})
+			if err != nil {
+				log.Println("sending error")
+				close(done)
+				return err
+			}
 		}
-		time.Sleep(time.Second)
 	}
 }
 
