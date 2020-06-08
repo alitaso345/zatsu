a750ecb69821fade06777148beb9a7472da13d39
alitaso345 <alice.maru345@gmail.com>
2020-06-08 20:31:28 +0900

    fix typo

--- grpc-web-samples/helloworld/client.js
+++ grpc-web-samples/helloworld/client.js
@@ -1,5 +1,5 @@
-const {HelloRequest, HelloReply} = require('./helloworld.proto')
-const {GreeterClient} = require('./helloworld_grpc_web_pb')
+const {HelloRequest, HelloReply} = require('./helloworld_pb.js')
+const {GreeterClient} = require('./helloworld_grpc_web_pb.js')
 
 const client = new GreeterClient('http://localhost:8080')
 
