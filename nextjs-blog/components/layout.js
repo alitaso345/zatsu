c84bfac17a59908e717e3493379f141a936872b0
alitaso345 <alice.maru345@gmail.com>
2020-06-18 08:47:36 +0900

    add layout

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,7 +1,54 @@
-import styles from './layout.module.css'
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>
+const name = "Your Name";
+export const siteTitle = "Next.js Sample Website";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.jpg"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <img
+                  src="/images/profile.jpg"
+                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  alt={name}
+                />
+              </a>
+            </Link>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
-
-export default Layout
--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,3 +1,5 @@
+import "../styles/global.css";
+
 export default function App({ Component, pagePropos }) {
-  return <Component {...pagePropos} />
+  return <Component {...pagePropos} />;
 }
