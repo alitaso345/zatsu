1256055d8001a13e057659fe8353fbe5f75cd5eb
alitaso345 <alitaso@pixiv.com>
2020-07-12 18:24:50 +0900

    simple rpc

--- user_service/go.mod
+++ user_service/go.mod
@@ -3,11 +3,19 @@ module github.com/alitaso345/zatsu/user_service
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/go-gorp/gorp v2.2.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang/protobuf v1.4.2
+	github.com/lib/pq v1.7.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
+	github.com/poy/onpar v1.0.0 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/ziutek/mymysql v1.5.4 // indirect
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
 	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 	golang.org/x/text v0.3.3 // indirect
 	google.golang.org/genproto v0.0.0-20200711021454-869866162049 // indirect
-	google.golang.org/grpc v1.30.0 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	google.golang.org/grpc v1.30.0
+	google.golang.org/protobuf v1.25.0
+	gopkg.in/gorp.v2 v2.2.0
 )
---
+++ user_service/server/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"net"
+	"time"
+
+	pb "github.com/alitaso345/zatsu/user_service/proto"
+	_ "github.com/mattn/go-sqlite3"
+	"google.golang.org/grpc"
+	"gopkg.in/gorp.v2"
+)
+
+var dbmap *gorp.DbMap
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func (service *UserService) CreateUser(ctx context.Context, request *pb.NewUserRequest) (*pb.UserResponse, error) {
+	new := newUser(request.Name)
+	err := dbmap.Insert(&new)
+	errorHandler(err, "Insert failed")
+
+	user := pb.User{Id: 1, Name: request.Name}
+	return &pb.UserResponse{User: &user}, nil
+}
+
+func main() {
+	dbmap = initDb()
+	defer dbmap.Db.Close()
+
+	lis, err := net.Listen("tcp", ":5051")
+	errorHandler(err, "failed to listen")
+
+	server := grpc.NewServer()
+	pb.RegisterUserServiceServer(server, &UserService{})
+	err = server.Serve(lis)
+	errorHandler(err, "failed to serve")
+}
+
+func initDb() *gorp.DbMap {
+	db, err := sql.Open("sqlite3", "./user_db.bin")
+	errorHandler(err, "sql.Open failed")
+
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
+	dbmap.AddTableWithName(User{}, "users").SetKeys(true, "Id")
+	err = dbmap.CreateTablesIfNotExists()
+	errorHandler(err, "Crate table failed")
+
+	return dbmap
+}
+
+type User struct {
+	Id      int64  `db:"user_id"`
+	Name    string `db:",size:50"`
+	Created int64
+}
+
+func newUser(name string) User {
+	return User{
+		Name:    name,
+		Created: time.Now().UnixNano(),
+	}
+}
+
+func errorHandler(err error, msg string) {
+	if err != nil {
+		log.Fatalln(err, msg)
+	}
+}
