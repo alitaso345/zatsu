7d718a62d96d9c04e4703257c4944546a219421a
alitaso345 <alice.maru345@gmail.com>
2020-07-24 14:31:32 +0900

    Get users list

--- user_service/client/pages/users/index.tsx
+++ user_service/client/pages/users/index.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from 'react'
+import { User } from 'proto/user_pb'
+import { UserServiceClient } from 'proto/UserServiceClientPb'
+import { Empty } from 'google-protobuf/google/protobuf/empty_pb'
+
 const Users: React.FC = () => {
-  return <h1>Users List</h1>
+  const apiEndpoint =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8080'
+      : 'http://' + window.location.host
+  const [users, setUsers] = useState<Array<User>>([])
+
+  useEffect(() => {
+    const userServiceClient = new UserServiceClient(apiEndpoint)
+    userServiceClient.getUsers(new Empty(), {}, (_, res) => {
+      const usersList = res.getUsersList()
+      setUsers(usersList)
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1>Users List</h1>
+      {users.map((user, index) => {
+        return (
+          <div key={index}>
+            <div>ID: {user.getId()}</div>
+            <div>Name: {user.getName()}</div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Users
