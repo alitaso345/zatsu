4f3220a571a31f146b606ed4af48f233ab545276
alitaso345 <alice.maru345@gmail.com>
2022-02-09 22:48:07 +0900

    list

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 import LinkList from './LinkList'
+import CreateLink from './CreateLink'
+import Header from './Header'
+import { Route, Routes } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return <LinkList />
-  }
+const App = () => {
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Routes>
+          <Route path="/" element={<LinkList />} />
+          <Route path="/create" element={<CreateLink />} />
+        </Routes>
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -4,14 +4,17 @@ import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client'
+import { BrowserRouter } from 'react-router-dom';
 
 const httpLink = createHttpLink({ uri: 'http://localhost:4000 '})
 const client = new ApolloClient({ link: httpLink, cache: new InMemoryCache })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>,
+  </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
