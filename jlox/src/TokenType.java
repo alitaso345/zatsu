1f9cdb2f81e0c8072d8fdbbb3a1054df721439be
alitaso345 <alice.maru345@gmail.com>
2023-11-03 22:48:30 +0900

    4.2.1

---
+++ jlox/src/TokenType.java
@@ -0,0 +1,18 @@
+enum TokenType {
+    // 記号1個のトークン
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, START,
+
+    // 起動1個または2個によるトークン
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // リテラル
+    IDENTIFIER, STRING, NUMBER,
+
+    // キーワード
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
